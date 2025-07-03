564d2b9e35f3f2522589d3bcb386bde62cead7eb
fahimaamir <fahimaamir72@gmail.com>
2025-07-03 14:56:28 +0500

    Add files via upload

--- jstable.py
+++ jstable.py
@@ -3,11 +3,20 @@ import streamlit.components.v1 as components
 from pivottablejs import pivot_ui
 import pandas as pd
 
-iris = pd.read_csv(
-    "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
-)
 
-t = pivot_ui(iris)
+data = {
+  "calories": [420, 380, 390],
+  "mnammm": ['fffffff','gfdgdfgdf','gfhfghfgh' ],
+  "duration": [50, 40, 45]
+  
+}
+
+#load data into a DataFrame object:
+df = pd.DataFrame(data)
+
+
+
+t = pivot_ui(df)
 
 with open(t.src) as t:
     components.html(t.read(), width=900, height=1000, scrolling=True)
